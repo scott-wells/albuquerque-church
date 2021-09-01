cd0b29e85fb8307187a818a95c4e674f889cfcc3
Scott Wells <45832584+scott-wells@users.noreply.github.com>
2021-08-31 21:48:56 -0700

    change WP url; connect WP to Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://albuquerquechurch.local/graphql`,
+          `http://scott-wells.com/albuquerque-church/graphql`,
       },
     },
 
