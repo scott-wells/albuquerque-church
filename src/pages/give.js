a3b2a6c590dcaaf2c475b541fd8092a119ab7868
Aaron Moreno <aaron.j.moreno@gmail.com>
2021-12-07 12:22:17 -0700

    updaed for nav fix

---
+++ src/pages/give.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Give = () => {
+    return(<h1>Give</h1>);
+}
+
+export default Give;
---
+++ src/pages/what-we-believe.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WhatWeBelieve = () => {
+    return(<h1>What We Believe</h1>)
+}
+
+export default WhatWeBelieve;
