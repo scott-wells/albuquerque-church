2b76fd6562e39bcc7cedf60558ee6cac98ca8aef
Aaron Moreno <aaron.j.moreno@gmail.com>
2021-12-09 14:21:38 -0700

    updated graphql query for example

--- src/pages/teaching.js
+++ src/pages/teaching.js
@@ -1,50 +1,33 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { Link, useStaticQuery, graphql } from "gatsby"
 import Layout from '../components/layout';
 
-const Teaching = () => {
+const Teaching = ({ data }) => {
     const [teachings, setTeachings] = useState(null);
 
+    console.log(data)
+
     useEffect(() => {
-        axios.get(`https://albuquerque-church.herokuapp.com/wp-json/wp/v2/posts`)
-        .then(res => {
-            console.log(res.data);
-            const teachings = res.data.filter(posts => {
-               return posts.slug.includes('teaching');
-            })
-
-            teachings.forEach(teaching => {
-                let audioLink = teaching.excerpt.rendered.replace(/<\/?p[^>]*>/g, "").replace(/(\r\n|\n|\r)/gm, "");
-                teaching.content.rendered = audioLink;
-            })
-
-            setTeachings(teachings);
-        })
+        console.log('mounted');
     },[])
 
     return(
         <Layout>
             <h1>TEACHINGS</h1>
-            {teachings &&
-                teachings.map(teaching => {
-                    return(
-                        <div key={teaching.id} className="mt-3">
-                            <h1>{teaching.title.rendered}</h1>
-                            <audio controls className='w-100 border'>
-                                <source src={teaching.centent.rendered} type="audio/ogg"></source>
-                                {/* <source src={teaching.content.rendered} type="audio/mpeg" /> */}
-                            </audio>
-                            {/* <div dangerouslySetInnerHTML={{__html: teaching.content.rendered}} /> */}
-                        </div>
-                        )
-                })
-            }
-
-            {!teachings &&
-                <h1>LOADING</h1>
-            }
         </Layout>
     )
 }
 
 export default Teaching;
+
+export const query = graphql`
+query data {
+    teachings: allSanityTeachings {
+      nodes{
+        title
+        description
+        audio
+      }
+    }
+  }
+`
